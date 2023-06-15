faccc27d938ff53cac54f0e65803e113d402b3cc
Eliana <elimendezcanil@gmail.com>
2023-06-15 11:33:37 -0600

    conexion en js

---
+++ index.js
@@ -0,0 +1,13 @@
+
+var fechaActual =new Date ();
+var horas =fechaActual.getHours();
+var minutos =fechaActual.getMinutes();
+var segundos =fechaActual.getSeconds();
+console.log (horas);
+console.log (fechaActual);
+console.log (minutos);
+console.log (segundos);
+
+var conexion =document.querySelector('#reloj')
+var horaReloj=horas +":" +minutos+ ":"+ segundos;
+conexion.innerHTML= horaReloj;
